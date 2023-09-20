040d4edc9c44f6fd526b919f84b73793013f7339
Sankalp Jain <sankalpjain99@gmail.com>
2023-09-20 23:05:20 +0530

    Initial Commit

---
+++ src/test/java/com/sankalp/redis/starter/RedisSpringbootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sankalp.redis.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringbootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
